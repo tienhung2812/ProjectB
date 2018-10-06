72078f420ee0cbf01f35c4f217b83ec091329535
Nguyen Tien Hung <hung.nguyentien97>
2018-10-06 15:29:57 +0700

    Done subForum Page

--- front-end/ride-hub/src/Routes.js
+++ front-end/ride-hub/src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Home from './components/views/home';
 import Channel from './components/views/subforum';
+import Thread from './components/views/thread';
 import App from './components/App';
 
 export default (
   <Route basename={process.env.PUBLIC_URL} path={'/'} component={App}>
     <IndexRoute component={Home} />
     <Route path={'/subforum/:id'} component={Channel} />
+    <Route path={'/thread/:id'} component={Thread}/>
     <Route path={'*'} component={Home} />
   </Route>
 );
--- front-end/ride-hub/src/components/views/thread.js
+++ front-end/ride-hub/src/components/views/thread.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { browserHistory } from 'react-router';
+import {Link as ReactLink} from 'react-router';
 import './view-stylesheet/thread.css';
-import avatar from '../../sampleSubForumIcon/cycle.png'
+import defaultimage from '../../defaultimage72x40.jpg'
 
 export default class Thread extends Component {
   constructor(props){
@@ -14,36 +15,58 @@ export default class Thread extends Component {
   }
   componentDidMount() {
     //Set state
-    this.setState({id:this.props.id})
-
-    if(this.props.url !== "home")
-      browserHistory.push('/post/'+this.props.id);
+    if(this.props.url !== "home" && this.props.url!=="subforum"){
+      browserHistory.push('/thread/'+this.props.params.id);
+      this.id = this.props.params.id;
+      this.setState({id:this.props.params.id});
+    }else{
+      this.id = this.props.id;
+      this.setState({id:this.props.id})
+    }
+      
 
     //Sample
-    this.title = "Post "+ String(this.props.id);
-
+    this.title = "Thread "+ String(this.id)
     //For display in HOMEPAGE
-    if (this.props.url==="home"){
-        this.content = <div className="thread"> 
-                            <button type="button" className="btn btn-link btn-thread">
-                            <div className="avatar">
-                                <img src={avatar} alt="SubForumAvatar"></img>
-                            </div>
-                            <div className="title">
-                                {this.title}
-                            </div>
-                            </button>
-                        </div>
-    }
+    
     
   }
 
   render() {
-    return (
-      <div>
-          {this.content}
+    if (this.props.url==="home"){
+      return(
+      <div className="thread"> 
+        <ReactLink to={'/thread/'+this.props.id}>
+        <button type="button" className="btn btn-link btn-thread">
+          <div className="threadImage">
+              <img src={defaultimage} alt="SubForumAvatar"></img>
+          </div>
+          <div className="title">
+              {this.title}
+          </div>
+          <div className="status">
+            <div className="username">
+              username
+            </div>
+            <div className="view-reply-wrapper">
+              <div className="view">
+                100 views
+              </div>
+              <div className="reply">
+                1000 replies
+              </div>
+            </div>
+          </div>
+          </button>
+        </ReactLink>
+          
       </div>
+      );
+  }else{
+    return(
+      <div></div>
     );
+  }
   }
 }
 
