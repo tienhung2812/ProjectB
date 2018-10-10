bdde810c5f1ee2c9707dba5dc3074c3229c213c7
Nguyen Tien Hung <hung.nguyentien97>
2018-10-10 09:15:44 +0700

    Account add button

--- front-end/ride-hub/src/components/app-components/header-components/AccountDropdown.js
+++ front-end/ride-hub/src/components/app-components/header-components/AccountDropdown.js
@@ -4,27 +4,48 @@ import '../../stylesheet/Header.css';
 class AccountDropDown extends Component {
   constructor(props){
     super(props);
-    this.state= {isLogged:false}
+    this.state= {isLogged:true, username:"anoymous", point:"0"}
  
   }
 
+  componentDidMount(){
+      this.childcontent=[];
+  }
+
+  
 
   render() {
+    //Set variable
+    if(!this.state.isLogged){
+        this.childcontent = []
+        this.childcontent.push(
+            <button className="dropdown-item" type="button" >Log in</button>
+        )
+        this.childcontent.push(
+            <button className="dropdown-item" type="button">Sign up</button>
+        );
+    }else{
+        this.childcontent = []
+        this.childcontent.push(
+            <button className="dropdown-item" type="button" >My Account</button>
+        )
+        this.childcontent.push(
+            <button className="dropdown-item" type="button">Log out</button>
+        );
+    }
 
     return (
         <div className="btn-group">
             <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div className="avatar"></div>
                 <div className="content">
-                    <div className="username">Username</div>
-                    <div className="point">100 points</div>
+                    <div className="username">{this.state.username}</div>
+                    <div className="point">{this.state.point} points</div>
                 </div>
                 
             </button>
             <div className="dropdown-menu dropdown-menu-right">
-                <button className="dropdown-item" type="button">Action</button>
-                <button className="dropdown-item" type="button">Another action</button>
-                <button className="dropdown-item" type="button">Something else here</button>
+                        {this.childcontent}
             </div>
         </div>
     );
